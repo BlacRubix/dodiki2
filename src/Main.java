3c077d3c18be577b37852b7780874d7bef9bcd11
panos <errorsfm@gmail.com>
2022-10-15 23:54:26 +0300

    Лаки готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
+    //
+
+
     public static void main(String[] args) {
+       String хуй = "хуй";
+        System.out.println(хуй);
         System.out.println("Hello world!");
     }
 }
